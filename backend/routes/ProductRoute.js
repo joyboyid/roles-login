06e2eb7a28e5c5580321c62d7c50f51ce7970316
joyboyid <ican.notfound@gmail.com>
2022-10-26 15:02:22 +0800

    backend crud login finishhh

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -5,15 +5,17 @@ import {
   getProductById,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 } from "../controllers/Products.js";
+
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/products', getProducts);
-router.get('/products/:id', getProductById);
-router.post('/products', createProduct);
-router.patch('/products', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get("/products", verifyUser, getProducts);
+router.get("/products/:id", verifyUser, getProductById);
+router.post("/products", verifyUser, createProduct);
+router.patch("/products/:id", verifyUser, updateProduct);
+router.delete("/products/:id", verifyUser, deleteProduct);
 
 export default router;
